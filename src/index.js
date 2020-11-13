74a93821f230550c66dfa58a94ba71e742ac7f30
sofiacwarren <sofiacwarren@gmail.com>
2020-11-13 12:08:45 -0600

    'updated index.js path for rootreducer'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { rootReducer } from './store/rootReducer';
+import { rootReducer } from './reducers/rootReducer';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';

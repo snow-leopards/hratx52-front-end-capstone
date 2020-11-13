e6d3ae4945488ac6a94ccf316dc2acc1e46fb75b
Kornelija Laucius <kornelija.janusaite@gmail.com>
2020-11-13 16:21:19 -0600

    added basic css style for the body + basic layout for overview

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectProduct } from '../reducers/overviewReducers';
+import { Layout } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 const Overview = () => {
 
   const product = useSelector(selectProduct);
 
   return (
-    <div>
-    This is Overview Section for {product}
-    </div>
+    <>
+      <Layout>
+        <Header id='siteWideMessage'>Site-wide annoucement message - sale / discount offer - new product highlight</Header>
+        <Layout>
+          <Content>
+            This is Overview Section for {product} images
+          </Content>
+          <Sider id='selectionTab'>
+            This is Overview Section for {product} selection
+          </Sider>
+        </Layout>
+        <Footer>This is Overview Section for {product} catchprase and description</Footer>
+      </Layout>
+    </>
+  // <div>
+  // This is Overview Section for {product}
+  // </div>
   );
 };
 

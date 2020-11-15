eb30ef588c76864fff4ade6f6198fff1a3fd8f3a
Kornelija Laucius <kornelija.janusaite@gmail.com>
2020-11-15 13:15:26 -0600

    carousel first pass

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectProduct, fetchProductInformation } from '../reducers/overviewReducers';
-import { Layout, Row, Col, Image, Descriptions } from 'antd';
+import { Layout, Row, Col, Descriptions } from 'antd';
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { ButtonBack, ButtonFirst, ButtonLast, ButtonNext,
+  CarouselProvider, DotGroup, Image, ImageWithZoom, Slide, Slider } from 'pure-react-carousel';
 
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -22,10 +25,81 @@ const Overview = (props) => {
         <Content>
           <Row >
             <Col span={14}>
-              <Image
+
+              {/* <Image
                 width={350}
                 src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg"
-              />
+              /> */}
+              <CarouselProvider
+                visibleSlides={1}
+                totalSlides={6}
+                step={1}
+                naturalSlideWidth={400}
+                naturalSlideHeight={500}
+                hasMasterSpinner
+              >
+                <CarouselProvider
+                  className='innerCarousel'
+                  visibleSlides={7}
+                  totalSlides={8}
+                  step={1}
+                  naturalSlideWidth={100}
+                  naturalSlideHeight={100}
+                  hasMasterSpinner
+                  orientation='vertical'
+                >
+                  <ButtonBack className='chevron'><UpOutlined /></ButtonBack>
+                  <Slider >
+                    <Slide index={0}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                    <Slide index={1}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                    <Slide index={2}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                    <Slide index={3}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                    <Slide index={4}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                    <Slide index={5}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                    <Slide index={6}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                    <Slide index={7}>
+                      <Image className='small-image' src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                    </Slide>
+                  </Slider>
+                  <ButtonNext className='chevron'><DownOutlined /></ButtonNext>
+                </CarouselProvider>
+                <Slider>
+                  <Slide index={0}>
+                    <ImageWithZoom src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                  </Slide>
+                  <Slide index={1}>
+                    <ImageWithZoom src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                  </Slide>
+                  <Slide index={2}>
+                    <ImageWithZoom src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                  </Slide>
+                  <Slide index={3}>
+                    <ImageWithZoom src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                  </Slide>
+                  <Slide index={4}>
+                    <ImageWithZoom src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                  </Slide>
+                  <Slide index={5}>
+                    <ImageWithZoom src="https://www.victoriassecret.com/p/760x1013/tif/56/77/56776f6e8c424ac68b4d91a900fbfd9c/111751764ZCK_OM_F.jpg" />
+                  </Slide>
+                </Slider>
+                <ButtonBack>Back</ButtonBack>
+                <ButtonNext>Next</ButtonNext>
+              </CarouselProvider>
             </Col>
             <Col span={10}>
               <Descriptions title="Product Info">

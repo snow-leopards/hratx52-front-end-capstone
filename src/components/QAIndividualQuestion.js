773bf1dc3e0af598e924b74b8a97fb8db3a3aa4a
David Lichter <lichter14@gmail.com>
2020-11-19 10:38:43 -0600

    Can toggle helpfulness button with a click

--- src/components/QAIndividualQuestion.js
+++ src/components/QAIndividualQuestion.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectQ, selectA, fetchQuestions } from '../reducers/QAReducers';
 import { selectProduct } from '../reducers/overviewReducers';
@@ -7,24 +7,45 @@ import dummyData from '../dummyData/QAListQuestionsData';
 
 const { Header, Footer, Sider, Content } = Layout;
 
-const IndividualQuestion = ( { question } ) => {
+const IndividualQuestion = ({ question }) => {
   const product = useSelector(selectProduct);
-  var answers = [];
+  const dispatch = useDispatch();
+  const answersState = useSelector(selectA);
+  var listOfAnswers = [];
   for (var key in question.answers) {
-    answers.push(question.answers[key]);
+    listOfAnswers.push(question.answers[key]);
   }
+  listOfAnswers.sort((a, b) => (a.helpfulness < b.helpfulness) ? 1 : -1);
 
-  answers.sort((a, b) => (a.helpfulness < b.helpfulness) ? 1 : -1);
+  useEffect(() => {
+    dispatch({type: 'SET_ANSWERS', payload: listOfAnswers});
+  }, []);
+
+  var incrementHelpfulness = (answer) => {
+    for (var i = 0; i < listOfAnswers.length; i++) {
+      if (listOfAnswers[i].id === answer.id && listOfAnswers[i].clickedHelpful === undefined) {
+        listOfAnswers[i].helpfulness = answer.helpfulness + 1;
+        listOfAnswers[i].clickedHelpful = true;
+      } else if ((listOfAnswers[i].id === answer.id && listOfAnswers[i].clickedHelpful === true)) {
+        listOfAnswers[i].helpfulness = answer.helpfulness - 1;
+        delete listOfAnswers[i].clickedHelpful;
+      }
+    }
+    dispatch({type: 'SET_ANSWERS', payload: listOfAnswers});
+  };
 
   return (
     <Layout>
       <Row className="questionBody" style={{fontWeight: 'bold'}}> Q: {question.question_body}</Row>
-      <Content>{answers.map((answer) => {
+      <Content>{listOfAnswers.map((answer) => {
         return (
           <div key={answer.id}>
             <Row key={answer.body} className="answerBody">A: {answer.body}</Row>
             <Row key={answer.date} className="answerDetails">by {answer.answerer_name}, {answer.date.slice(5, 8)}{answer.date.slice(8, 10)}-{answer.date.slice(0, 4)}</Row>
-            <Row key={'helpful' + answer.id}>Helpful? Yes ({answer.helpfulness})</Row>
+            <button key={'helpful' + answer.id} onClick={() => {
+              incrementHelpfulness(answer);
+            }}
+            >Helpful? Yes({answer.helpfulness})</button>
           </div>
         );
       })}
@@ -34,3 +55,4 @@ const IndividualQuestion = ( { question } ) => {
 };
 
 export default IndividualQuestion;
+

b565db0583f5721d488be9145bf0162bc76d2fcd
Jesse Pye <jessepye@gmail.com>
2020-11-23 16:09:34 -0600

    first draft of Sale information

--- src/components/RelatedItem.js
+++ src/components/RelatedItem.js
@@ -5,15 +5,17 @@ const RelatedItem = ({ relatedProductID }) => {
   const placeHolderImgURL = 'https://via.placeholder.com/150';
   const [productName, setProductName] = useState('Loading...');
   const [category, setCategory] = useState('Category');
-  const [basePrice, setBasePrice] = useState('0');
-  const [salePrice, setSalePrice] = useState('0');
+  const [originalPrice, setOriginalPrice] = useState('-999');
+  const [salePrice, setSalePrice] = useState('-999');
   const [imgURL, setImgURL] = useState(placeHolderImgURL);
 
   const getProductInfo = () => {
     fetch(`http://3.21.164.220/products/${relatedProductID}`)
       .then(productInfo => productInfo.json())
       .then((productInfo) => {
+        console.log('productinfo:', productInfo);
         setProductName(productInfo.name);
+        setCategory(productInfo.category);
       })
       .catch(() => {
         console.log(`Error fetching related product info for product with id ${relatedProductID}:`, err);
@@ -32,12 +34,17 @@ const RelatedItem = ({ relatedProductID }) => {
           if (style['default?'] === 1) {
             foundADefaultStyle = true;
             setImgURL(style.photos[0].thumbnail_url ? style.photos[0].thumbnail_url : placeHolderImgURL); // TODO: search for a picture somewhere else if one isn't found
+            setOriginalPrice(style.original_price);
+            setSalePrice(style.sale_price);
+
             break;
           }
         }
         if (!foundADefaultStyle) { // TODO: search for a picture somewhere else
           // console.log('Item', relatedProductID, 'didn\'t have a default style');
           setImgURL(results[0].photos[0].thumbnail_url ? results[0].photos[0].thumbnail_url : placeHolderImgURL);
+          setOriginalPrice(results[0].original_price);
+          setSalePrice(results[0].sale_price);
         }
       })
       .catch((err) => {
@@ -62,12 +69,17 @@ const RelatedItem = ({ relatedProductID }) => {
           <img
             src={imgURL}
             alt={productName}
-            height={150}
+            height={200}
           />
         </div>
         <div>{category}</div>
         <div>{productName}</div>
-        <div>${basePrice}</div>
+        <div>
+          {salePrice > 0
+            ? <span>Sale! ${salePrice}</span> // TODO strikethrough original price
+            : <span>${originalPrice}</span>
+          }
+        </div>
       </Card>
     </a>
   );

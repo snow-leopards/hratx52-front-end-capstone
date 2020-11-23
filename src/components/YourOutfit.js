59a54fe00b231fe19a8f5dc10968116b7833f972
Jesse Pye <jessepye@gmail.com>
2020-11-23 16:19:31 -0600

    added files for Your Outfit

---
+++ src/components/YourOutfit.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const YourOutfit = ({productId}) => {
+  return (
+    <div class="YourOutfit">
+      Your Outfit!
+    </div>
+  );
+};
+
+export default YourOutfit;
